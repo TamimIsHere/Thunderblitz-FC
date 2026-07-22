caa19308b7dd5bb098077e91d9b0d3af814636a7
Tamim <mushfictamim64@gmail.com>
2026-07-23 04:49:40 +0600

    fix back button navigation

--- android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
+++ android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
@@ -1,5 +1,15 @@
 package com.thunderblitz.fc;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (this.bridge.getWebView().canGoBack()) {
+            this.bridge.getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
