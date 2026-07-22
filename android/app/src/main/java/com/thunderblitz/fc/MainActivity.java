aec9f1bb8a97d257389ade8d3b0dc96f8d4fdcc3
Tamim <mushfictamim64@gmail.com>
2026-07-23 05:06:49 +0600

    fix webview viewport scaling

--- android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
+++ android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
@@ -1,9 +1,19 @@
 package com.thunderblitz.fc;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebSettings settings = this.bridge.getWebView().getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(false);
+    }
+
     @Override
     public void onBackPressed() {
         if (this.bridge.getWebView().canGoBack()) {
