8de132952508783eae01be37f56541fd32adc7b9
Tamim <mushfictamim64@gmail.com>
2026-07-23 05:36:33 +0600

    keep splash until page fully loaded

--- android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
+++ android/app/src/main/java/com/thunderblitz/fc/MainActivity.java
@@ -2,17 +2,29 @@ package com.thunderblitz.fc;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.plugin.SplashScreen;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WebSettings settings = this.bridge.getWebView().getSettings();
         settings.setUseWideViewPort(false);
         settings.setLoadWithOverviewMode(false);
         settings.setSupportZoom(false);
         settings.setTextZoom(100);
+
+        this.bridge.getWebView().setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                bridge.getApp().getPlugin("SplashScreen");
+            }
+        });
     }
 
     @Override
